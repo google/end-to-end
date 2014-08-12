976b910ebd6932f83b0d8f25db95553ec87e5eb8
Drew Hintz <adhintz@google.com>
2014-08-12 16:10:17 -0700

    DH KEY Message ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=72774010

---
+++ javascript/crypto/e2e/otr/message/dhkey.js
@@ -0,0 +1,76 @@
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/**
+ * @fileoverview Defines the DH KEY type used in the OTR protocol.
+ *
+ * @author rcc@google.com (Ryan Chan)
+ */
+
+goog.provide('e2e.otr.message.DhKey');
+
+goog.require('e2e');
+goog.require('e2e.BigNumModulus');
+goog.require('e2e.cipher.DiffieHellman');
+goog.require('e2e.otr.Mpi');
+goog.require('e2e.otr.constants');
+goog.require('e2e.otr.error.NotImplementedError');
+goog.require('e2e.otr.message.Message');
+goog.require('e2e.random');
+
+
+goog.scope(function() {
+
+var constants = e2e.otr.constants;
+
+
+/**
+ * An OTRv3 DH KEY.
+ * @constructor
+ * @extends {e2e.otr.message.Message}
+ * @param {e2e.otr.Int} sender The sender's instance tag.
+ * @param {e2e.otr.Int} receiver The receiver's instance tag.
+ */
+e2e.otr.message.DhKey = function(sender, receiver) {
+  goog.base(this, sender, receiver);
+  this.dh = new e2e.cipher.DiffieHellman(constants.DH_MODULUS,
+      [constants.DH_GENERATOR]);
+};
+goog.inherits(e2e.otr.message.DhKey, e2e.otr.message.Message);
+
+
+/**
+ * Specifies the type of the message.
+ * @type {!e2e.otr.Byte}
+ */
+e2e.otr.message.DhKey.MESSAGE_TYPE = constants.MessageType.DH_KEY;
+
+
+/**
+ * Serialize the DH KEY into a Uint8Array.
+ * @return {!Uint8Array} The serialized DH KEY.
+ */
+e2e.otr.message.DhKey.prototype.serializeMessageContent = function() {
+  return new e2e.otr.Mpi(new Uint8Array(this.dh.generate())).serialize();
+};
+
+
+/**
+ * Processes a DH KEY message.
+ * @param {!e2e.otr.Session} session The enclosing session.
+ * @param {!Uint8Array} data The data to be processed.
+ */
+e2e.otr.message.DhKey.process = function(session, data) {
+  throw new e2e.otr.error.NotImplementedError('This is not yet implemented.');
+};
+});
---
+++ javascript/crypto/e2e/otr/message/dhkey_test.js
@@ -0,0 +1,50 @@
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/**
+ * @fileoverview Defines tests for the DH KEY type used in the OTR protocol.
+ *
+ * @author rcc@google.com (Ryan Chan)
+ */
+
+goog.require('e2e.otr.error.ParseError');
+goog.require('e2e.otr.message.DhKey');
+goog.require('e2e.otr.testing');
+goog.require('goog.testing.PropertyReplacer');
+goog.require('goog.testing.asserts');
+goog.require('goog.testing.jsunit');
+
+goog.setTestOnly();
+
+
+var stubs = new goog.testing.PropertyReplacer();
+var commit = null;
+var sender = new Uint8Array([0x00, 0x00, 0x01, 0x00]);
+var receiver = new Uint8Array([0x00, 0x00, 0x02, 0x00]);
+
+function setUp() {
+  commit = new e2e.otr.message.DhKey(sender, receiver);
+  stubs.setPath('e2e.random.getRandomBytes', goog.array.range);
+}
+
+function tearDown() {
+  stubs.reset();
+  commit = null;
+}
+
+function testSerializeMessageContent() {
+  var out = commit.serializeMessageContent();
+  assertEquals(196, out.length);
+
+  // TODO(user): More tests, check against reference implementation.
+}
